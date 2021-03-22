dc93414b5a27bb94619f656d5232821b006c492c
merveguzl <merveguzel.iletisim@gmail.com>
2021-03-22 09:45:20 +0300

    -0

--- App.js
+++ App.js
@@ -1,11 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import CardMatch from './src/CardMatch';
 import CardMatchBody from './src/CardMatchBody';
-import FlagContainer from './src/Flag/FlagContainer';
-import Game from './src/Game';
-
 export default function App() {
   return (
       <CardMatchBody />
